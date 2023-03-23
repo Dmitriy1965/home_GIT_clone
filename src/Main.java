f1db70468f5341db395d70ee3c87aa292b0f803f
Dmitriy1965 <grek00@mail.ru>
2023-03-23 12:22:36 +0300

    append stat

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         int[] sales = {100, 300, 200};
         SalesManager salesManager = new SalesManager(sales);
         int max = salesManager.max();
+        int min = salesManager.min();
+        int stat = salesManager.stat();
+
         System.out.println(max);
+        System.out.println(min);
+        System.out.println(stat);
+
     }
 }
